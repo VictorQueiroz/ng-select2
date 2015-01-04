f2b7a2a70f0aeb212c44f1620d6839f36db5729d
Victor Queiroz <victorcqueirozg@gmail.com>
2015-01-04 00:26:41 -0300

    chore(gulpfile.js): corrected minified inject file

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var karma = require('karma').server;
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var ngAnnotate = require('gulp-ng-annotate');
 
 var paths = {
 	scripts: ['src/**/*.js', '!src/**/*_test.js'],
@@ -34,6 +35,7 @@ gulp.task('jshint', function () {
 
 gulp.task('scripts', ['jshint'], function () {
 	gulp.src(paths.scripts)
+		.pipe(ngAnnotate())
 		.pipe(uglify())
 		.pipe(concat('ng-select2.js'))
 		.pipe(gulp.dest('dist'));
